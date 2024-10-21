9f18467f63d0ae873a83df64c732fbcfc79b2e7b
Egor <ega.nik5674@gmail.com>
2024-10-21 21:48:45 +0300

    Update session.py

--- src/session.py
+++ src/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy.orm import joinedload
 
 
 async def create(async_session: async_sessionmaker[AsyncSession], obj) -> None:
@@ -8,16 +9,23 @@ async def create(async_session: async_sessionmaker[AsyncSession], obj) -> None:
             session.add(obj)
 
 
-async def read_all(async_session: async_sessionmaker[AsyncSession], model, skip: int = 0, limit: int = 10):
+async def read_all(async_session: async_sessionmaker[AsyncSession], model, skip: int = 0, limit: int = 10,
+                   mode: str = 'category'):
     async with async_session() as session:
-        stmt = select(model).offset(skip).limit(limit)
+        if mode == 'products':
+            stmt = select(model).options(joinedload(model.category)).offset(skip).limit(limit)
+        else:
+            stmt = select(model).offset(skip).limit(limit)
         result = await session.execute(stmt)
         return result.scalars().all()
 
 
-async def read_one(async_session: async_sessionmaker[AsyncSession], model, instance_id: int):
+async def read_one(async_session: async_sessionmaker[AsyncSession], model, instance_id: int, mode: str = 'category'):
     async with async_session() as session:
-        stmt = select(model).where(model.id == instance_id)
+        if mode == 'products':
+            stmt = select(model).where(model.id == instance_id)
+        else:
+            stmt = select(model).options(joinedload(model.category)).where(model.id == instance_id)
         result = await session.execute(stmt)
         return result.scalars().first()
 
