723d7665342f7d7a5a9f7ee06098df5605582853
Egor <ega.nik5674@gmail.com>
2024-10-21 21:00:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import aiohttp.web as web
 from src.routers.categories import routes as category_routes
+from src.routers.products import routes as product_routes
 
 if __name__ == '__main__':
     app = web.Application()
@@ -7,5 +8,9 @@ if __name__ == '__main__':
     category_app = web.Application()
     category_app.add_routes(category_routes)
     app.add_subapp('/categories', category_app)
+
+    product_app = web.Application()
+    product_app.add_routes(product_routes)
+    app.add_subapp('/products', product_app)
 
     web.run_app(app, port=8080)
